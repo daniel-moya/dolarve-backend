d32c38938a84161c896381b1a10531293c379de5
damr67 <damr67@gmail.com>
2019-10-13 18:33:08 -0400

    initial commit - running server

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const compression = require('compression');
+
+const routes = require('./routes'); 
+
+function start() {
+  try {
+    const app = express();
+    const port = process.env.PORT || 5000;
+    
+    app.use(bodyParser.urlencoded({
+      extended: true
+    }));
+    
+    app.use(bodyParser.json());
+    app.use(cookieParser());
+    app.use(cors());
+    app.use(compression());
+
+    // initialize routing
+    app.use(routes);
+    
+    // Previous implementation. Maybe we want to run this when developing.
+    app.listen(port, () => console.log(`Server is listening on port ${port}`)); 
+  } catch(error) {
+    console.log(error? error.message: 'Error: something unexpected happened'); // TODO: use logger
+  }
+} 
+
+// app starts here
+start();
