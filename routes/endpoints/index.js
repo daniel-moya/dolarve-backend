a2119e9074c61ab4085ed1b9cd888f9f4055e0e7
damr67 <damr67@gmail.com>
2019-10-13 19:00:12 -0400

    fixing config files

--- routes/endpoints/index.js
+++ routes/endpoints/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const config = require('config');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  res.status(200).send('Dolar ve API');
+ });
+
 router.get('/exchange/:name', async (req, res) => {
-  res.status(200).send(`exchange ${req.params.name}`);
+ if(req.params.name.toLocaleLowerCase() === config.exchanges.dolar_today.endpoint) {
+  res.status(200).send(`exchange ${req.params.name.toLocaleLowerCase()}`);
+ }
+ res.send('Exchange no posse actualmente soporte');
 });
 
 module.exports = router
