67fa844b188afcfc3761c303d4d94c4bb37de235
damr67 <damr67@gmail.com>
2019-10-13 20:06:44 -0400

    dolartoday endpoint

--- routes/endpoints/index.js
+++ routes/endpoints/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const config = require('config');
 
+const request = require('request');
+const cheerio = require('cheerio');
+
 const router = express.Router();
 
 router.get('/', async (req, res) => {
@@ -8,10 +11,19 @@ router.get('/', async (req, res) => {
  });
 
 router.get('/exchange/:name', async (req, res) => {
- if(req.params.name.toLocaleLowerCase() === config.exchanges.dolar_today.endpoint) {
-  res.status(200).send(`exchange ${req.params.name.toLocaleLowerCase()}`);
- }
- res.send('Exchange no posse actualmente soporte');
+  
+  if(req.params.name.toLocaleLowerCase() === config.exchanges.dolar_today.endpoint) {
+    const url = config.exchanges.dolar_today.url;
+    
+    request(url, (err, response, html) => {
+      if (!err && response.statusCode == 200) {
+        const $ = cheerio.load(html);
+    
+        const data = ($('p.ProfileHeaderCard-bio ')).text().split(' ');
+        res.status(200).send(data[data.indexOf('Bs.') + 1]);
+      }
+    });
+  }
 });
 
 module.exports = router
